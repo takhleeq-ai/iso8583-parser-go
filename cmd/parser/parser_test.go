cf417c0a614aab3b9bca5b8bfbb43ab4f7a258fd
Syed Ahsan Subhan <Subhan.Ahsan@gmail.com>
2025-09-17 10:54:42 +0100

    Unit Tests Updated

--- cmd/parser/parser_test.go
+++ cmd/parser/parser_test.go
@@ -10,3 +10,23 @@ func TestParseMessage(t *testing.T) {
 		t.Errorf("Expected 4 fields, got %d", len(ParseMessage(msg)))
 	}
 }
+func TestVatidateFields(t *testing.T) {
+	validFields := []string{"0200", "1234567890123456", "000000100000", "20250916083000"}
+	invalidFields := []string{"0200", "1234567890123456"}
+
+	if err := ValidateFields(validFields); err != nil {
+		t.Errorf("Expected no error for valid fields, got %v", err)
+	}
+	if err := ValidateFields(invalidFields); err == nil {
+		t.Errorf("Expected error for invalid fields, got none")
+	}
+}
+
+func TestMapFieldsToStruct(t *testing.T) {
+	fields := []string{"0200", "1234567890123456", "000000100000", "20250916083000"}
+	msg := MapFieldsToStruct(fields)
+
+	if msg.MTI != "0200" || msg.PAN != "1234567890123456" || msg.Amount != "000000100000" || msg.TransmissionDateTime != "20250916083000" {
+		t.Errorf("Mapping failed, got %+v", msg)
+	}
+}
